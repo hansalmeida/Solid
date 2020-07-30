b2ff48a8da2ef16dce0c91f9365f0d76ad3999e8
Snahier <hansalmeida@hotmail.com>
2020-07-29 22:29:58 -0300

    Add database mock repository

---
+++ src/repositories/implementations/DbUsersRepository.ts
@@ -0,0 +1,17 @@
+import { UsersRepository } from "../UsersRepository"
+import { User } from "../../entities/User"
+
+export class DbUsersRepository implements UsersRepository {
+  // Mock DB
+  private users: User[] = []
+
+  async findByEmail(email: string): Promise<User> {
+    // Get users from the mock DB
+    const users = this.users.find((user) => user.email == email)
+  }
+
+  async save(user: User): Promise<void> {
+    // Save user in DB mock
+    this.users.push(user)
+  }
+}
