d66a58a2a5a54916cc1e042cee98084732f4b20f
Snahier <hansalmeida@hotmail.com>
2020-07-29 22:05:39 -0300

    Add create new user logic

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,3 +1,19 @@
+import { UsersRepository } from "../../repositories/UsersRepository"
+import { CreateUserRequestDTO } from "./CreateUserDTO"
+import { User } from "../../entities/User"
+
 export class CreateUserUseCase {
-  async execute() {}
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute(data: CreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists")
+    }
+
+    const user = new User(data)
+
+    await this.usersRepository.save(user)
+  }
 }
